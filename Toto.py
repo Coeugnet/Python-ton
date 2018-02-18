65046e812b4a1cbdcc9509cbf2e66d1cd6f14515
Benoit <benoit@coeugnet.com>
2018-02-18 22:36:37 +0100

    4e

--- Toto.py
+++ Toto.py
@@ -1,10 +1,22 @@
 from tkinter import *
+import string
 
 fenetre = Tk()
+colonne = string.ascii_uppercase[:10]
 for numéroRangée in range(11):
 	for numéroColonne in range(11):
-		carré = Button(fenetre,width=6,height=3,bg="white")
-		carré.grid(row=numéroRangée,column=numéroColonne)
+		if numéroColonne == 0 and numéroRangée == 0:
+			carré = Button(fenetre,width=6,height=3,text=numéroRangée,bg="black")
+			carré.grid(row=numéroRangée,column=numéroColonne)
+		elif numéroColonne == 0 and numéroRangée != 0:
+			carré = Button(fenetre,width=6,height=3,text=numéroRangée,bg="#ccff66")
+			carré.grid(row=numéroRangée,column=numéroColonne)
+		elif numéroRangée == 0 and numéroColonne != 0:
+			carré = Button(fenetre,width=6,height=3,text=colonne[numéroColonne-1],bg="#ccff66")
+			carré.grid(row=numéroRangée,column=numéroColonne)
+		else:
+			carré = Button(fenetre,width=6,height=3,bg="#4271f4")
+			carré.grid(row=numéroRangée,column=numéroColonne)
 
 
 mainloop()
