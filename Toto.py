82e2b933f2ae599125821ce2ec5a23bdf9efb16f
Benoit <benoit@coeugnet.com>
2018-02-18 22:19:21 +0100

    3e

--- Toto.py
+++ Toto.py
@@ -1,1 +1,10 @@
 from tkinter import *
+
+fenetre = Tk()
+for numéroRangée in range(11):
+	for numéroColonne in range(11):
+		carré = Button(fenetre,width=6,height=3,bg="white")
+		carré.grid(row=numéroRangée,column=numéroColonne)
+
+
+mainloop()
